e76f11db115d0a27bf266246513fb9ffcbe053e7
YoshiokaTsuyoshi <98578587+YoshiokaTsuyoshi@users.noreply.github.com>
2024-03-09 12:49:20 +0900

    物理挙動未完成

--- Game_Suika.py
+++ Game_Suika.py
@@ -7,7 +7,7 @@ from pygame.sprite import AbstractGroup
 
 SCREEN_SIZE = (400, 600)
 BALL_SIZE = [15, 20, 25, 30, 35, 40, 45, 50, 60]
-FRAME_RATE = 30
+FRAME_RATE = 25
 
 class Ball(pg.sprite.Sprite):
     ballList = []
@@ -19,19 +19,40 @@ class Ball(pg.sprite.Sprite):
         self.image = pg.Surface((self.radius * 2, self.radius * 2))
         pg.Surface.fill(self.image, (0, 0, 0))
         pg.draw.circle(self.image, (255, 0, 0), (self.radius, self.radius), self.radius)
+        self.imageCopy = self.image.copy()
         self.rect = pg.Rect(pos[0] - self.radius, pos[1] - self.radius, self.radius * 2, self.radius * 2)
         self.image.set_colorkey((0, 0, 0))
         self.speed = [0, 0]
         self.force = [0, 9.8]
         self.tempForce = [0, 0]
+        self.angle = 0
+        self.wallTouchFlag = [0, 0]
+        self.fixedFlag = False
         self.fallFlag = False
         self.ballListIndex = index
 
     def update(self, *args, **kwargs):
         if self.fallFlag:
 
-            self.rect.x += self.speed[0] * self.updateTime + (self.force[0] + self.tempForce[0]) * self.updateTime**2
-            self.rect.y += self.speed[1] * self.updateTime + (self.force[1] + self.tempForce[1]) * self.updateTime**2
+            tempX = self.speed[0] * self.updateTime + (self.force[0] + self.tempForce[0]) * self.updateTime**2
+            if self.wallTouchFlag[0] == 1:
+                if tempX < 0:
+                    tempX = 0
+                else:
+                    self.wallTouchFlag[0] = 0
+            elif self.wallTouchFlag[0] == 2:
+                if tempX > 0:
+                    tempX = 0
+                else:
+                    self.wallTouchFlag[0] = 0
+            self.rect.x += tempX
+            tempY = self.speed[1] * self.updateTime + (self.force[1] + self.tempForce[1]) * self.updateTime**2
+            if self.wallTouchFlag[1] == 1:
+                if tempY > 0:
+                    tempY = 0
+                else:
+                    self.wallTouchFlag[1] = 0
+            self.rect.y += tempY
 
             self.tempForce[0] = 0
             self.tempForce[1] = 0
@@ -41,17 +62,27 @@ class Ball(pg.sprite.Sprite):
                 tempR = math.sqrt((i.rect.centerx - self.rect.centerx)**2 + (i.rect.centery - self.rect.centery)**2) + sys.float_info.min
                 tempSin = (i.rect.centerx - self.rect.centerx) / tempR
                 tempCos = (i.rect.centery - self.rect.centery) / tempR
-                self.rect.x = i.rect.centerx - (i.rect.w * 0.5 + self.radius) * tempSin - self.radius
-                self.rect.y = i.rect.centery - (i.rect.w * 0.5 + self.radius) * tempCos - self.radius
+                if self.wallTouchFlag[0]:
+                    pass
+                else:
+                    self.rect.x = i.rect.centerx - (i.rect.w * 0.5 + self.radius) * tempSin - self.radius
+                if self.wallTouchFlag[1]:
+                    pass
+                else:
+                    self.rect.y = i.rect.centery - (i.rect.w * 0.5 + self.radius) * tempCos - self.radius
                 tempR = math.sqrt((i.rect.centerx - self.rect.centerx)**2 + (i.rect.centery - self.rect.centery)**2) + sys.float_info.min
                 tempSin = (i.rect.centerx - self.rect.centerx) / tempR
                 tempCos = (i.rect.centery - self.rect.centery) / tempR
-                self.tempForce[0] = -self.force[1] * tempSin / (abs(tempSin) + tempCos + sys.float_info.min)
-                self.tempForce[1] = -self.force[1] * tempCos / (abs(tempSin) + tempCos + sys.float_info.min)
+                if self.wallTouchFlag[0]:
+                    pass
+                else:
+                    self.tempForce[0] = -self.force[1] * tempSin / (abs(tempSin) + tempCos + sys.float_info.min)
+                if self.wallTouchFlag[1]:
+                    pass
+                else:
+                    self.tempForce[1] = -self.force[1] * tempCos / (abs(tempSin) + tempCos + sys.float_info.min)
                 i.tempForce[0] = -self.tempForce[0]
                 i.tempForce[1] = -self.tempForce[1]
-                self.speed[0] = -self.speed[1] * tempSin / (abs(tempSin) + tempCos + sys.float_info.min) * 0.5
-                self.speed[1] = -self.speed[1] * tempCos / (abs(tempSin) + tempCos + sys.float_info.min) * 0.5
 
     def fixedUpdate(self):
         if self.fallFlag:
@@ -62,21 +93,26 @@ class Ball(pg.sprite.Sprite):
             self.speed[0] = self.speed[0] + (self.force[0] + self.tempForce[0]) * self.updateTime
             self.speed[1] = self.speed[1] + (self.force[1] + self.tempForce[1]) * self.updateTime
 
+            self.wallTouchFlag = [0, 0]
+
             if self.rect.centery >= SCREEN_SIZE[1] - self.radius:
                 self.rect.y = SCREEN_SIZE[1] - self.radius * 2
                 self.speed[0] *= 0.6
                 self.speed[1] = 0
                 self.tempForce[1] = -self.force[1]
+                self.wallTouchFlag[1] = 1
             if self.rect.centery <= self.radius:
                 self.rect.y = 0
             if self.rect.centerx <= self.radius:
                 self.rect.x = 0
                 self.tempForce[0] = 0
                 self.speed[0] *= -1
+                self.wallTouchFlag[0] = 1
             if self.rect.centerx >= SCREEN_SIZE[0] - self.radius:
                 self.rect.x = SCREEN_SIZE[0] - self.radius * 2
                 self.tempForce[0] = 0
                 self.speed[0] *= -1
+                self.wallTouchFlag[0] = 2
 
     def collideList(self):
         indexList = []
